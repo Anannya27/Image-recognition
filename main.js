dd418842868d8116e149b728c4f591643803ebec
Anannya27 <104672818+Anannya27@users.noreply.github.com>
2022-07-22 16:57:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,14 +11,31 @@ Webcam.attach( '#camera' );
 
 function take_snapshot()
 {
-    Webcam.snap(function(data_url){
-        document.getElementById("result").innerHTML = '<img id="capured" src="'+data_url+'"/>';
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capured_image" src="'+data_uri+'"/>';
     });
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t61n6nIA7/model.json',modlLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t61n6nIA7/model.json',modelLoaded);
 
-function modlLoaded (){
+function modelLoaded (){
     console.log("model_loaded");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("results_object_name").innerHTM = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+    }
 }
